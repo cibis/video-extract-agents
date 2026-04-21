64a4482a283968ab237d5564bffaca7400e7a861
cibis <cibotari@gmail.com>
2026-04-20 20:05:14 -0400

    gitlab

--- frontend/angular-shell/src/environments/environment.ts
+++ frontend/angular-shell/src/environments/environment.ts
@@ -3,8 +3,8 @@ export const environment = {
   version: '1.0.0-beta',
   skipAuth: '${LOCAL_DEV_SKIP_AUTH}',
   apiUrl: 'http://localhost:8000',
-  librechatUrl: 'http://localhost:3080',
-  librechatOrigin: 'http://localhost:3080',
+  librechatUrl: '/chat',
+  librechatOrigin: '${APP_BASE_URL}',
   msalConfig: {
     clientId: '${AZURE_ENTRA_CLIENT_ID}',
     authority: 'https://login.microsoftonline.com/${AZURE_ENTRA_TENANT_ID}',
